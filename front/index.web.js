0f7f54257e6f4506a093e368f825f04ccbc760a6
JstLearn <JstLearn@users.noreply.github.com>
2026-03-28 00:27:49 +0300

    chore: remove startup console.log from index.web.js

--- front/index.web.js
+++ front/index.web.js
@@ -2,11 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './front';
 import { name as appName } from './app.json';
 
-// Development modunda başlangıç logu
-if (process.env.NODE_ENV === 'development') {
-  console.log('React uygulaması başlatılıyor...');
-}
-
 AppRegistry.registerComponent(appName, () => App);
 
 AppRegistry.runApplication(appName, {
